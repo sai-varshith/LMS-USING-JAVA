e72a80c38022aab00abb1da3f75312ac951fd60f
sai-varshith <84182977+sai-varshith@users.noreply.github.com>
2022-06-28 16:01:43 +0530

    Update Book_issue.java

--- src/Book_issue.java
+++ src/Book_issue.java
@@ -1,12 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JInternalFrame.java to edit this template
- */
 
-/**
- *
- * @author kunch
- */
 import java.sql.*;
 import java.text.SimpleDateFormat;
 import javax.swing.JOptionPane;
