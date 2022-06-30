6bdd27099ee1e5da708c3ac65d89cb6677ebeae3
sai-varshith <84182977+sai-varshith@users.noreply.github.com>
2022-06-30 15:20:19 +0530

    Update Home_page.java

--- src/Home_page.java
+++ src/Home_page.java
@@ -1,12 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 
-/**
- *
- * @author kunch
- */
 public class Home_page extends javax.swing.JFrame {
 
     /**
