6783547e2dd41a0550c96155ad1dd082fd6d386f
sai-varshith <84182977+sai-varshith@users.noreply.github.com>
2022-06-30 15:19:58 +0530

    Update Forgot.java

--- src/Forgot.java
+++ src/Forgot.java
@@ -1,12 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 
-/**
- *
- * @author prudhvi k
- */
 public class Forgot extends javax.swing.JFrame {
 
     /**
