0de26d573996e1ced32c7a68abb2877d926d4a71
sai-varshith <84182977+sai-varshith@users.noreply.github.com>
2022-07-02 21:10:59 +0530

    Update display_books.java

--- src/display_books.java
+++ src/display_books.java
@@ -1,12 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JInternalFrame.java to edit this template
- */
 
-/**
- *
- * @author kunch
- */
 
 
 import static java.lang.Class.forName;
