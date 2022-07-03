d0f0c7a49a1e7a12f9e81223309682ae761c884c
sai-varshith <84182977+sai-varshith@users.noreply.github.com>
2022-07-03 23:13:25 +0530

    Update LoginTest.java

--- test/LoginTest.java
+++ test/LoginTest.java
@@ -10,10 +10,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- *
- * @author prudhvi k
- */
 public class LoginTest {
     
     public LoginTest() {
