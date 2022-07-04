f508159d2cfbca1fdfc50ce59763651c8e562dbe
sai-varshith <84182977+sai-varshith@users.noreply.github.com>
2022-07-04 14:31:12 +0530

    Update LoginTest.java

--- test/LoginTest.java
+++ test/LoginTest.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
- */
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
